68e739d92a1ae7e46bc728a5ccc95c9d0a774a5a
Pontus Malm <malm@chalmers.it>
2018-03-01 17:21:52 +0100

    Route constraint syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'list' => 'print#list_printers', constraints: lambda { |req| req.format == :json }, as: :list
+  get 'list' => 'print#list_printers', constraints: -> (req) { req.format == :json }, as: :list
   post 'print' => 'print#create', as: :prints
   post 'pq' => 'print#pq', as: :pq_print
 end
